e138319766ccc0f340067ab37f7cc2193d34b207
Alexandru Busuioc <busuioc.alexandru@gmail.com>
2020-12-02 12:10:59 +0000

    v1

---
+++ src/Formatter/LogstashStrictFormatter.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Alexbusu\Monolog\Formatter;
+
+use Monolog\Formatter\LogstashFormatter;
+
+class LogstashStrictFormatter extends LogstashFormatter
+{
+    public function format(array $record): string
+    {
+        if (array_key_exists('channel', $record) && 'doctrine' === $record['channel']) {
+            // The `context` key should contain a map of ["string" => "mixed"] pairs, resulting in a JSON object.
+            // Doctrine component puts the bind list values in `context`, thus resulting in a JSON array;
+            //  This results in map setting conflicts in the Elasticsearch's index.
+            if (array_key_exists('context', $record)) {
+                $record['context'] = ['binds' => var_export($record['context'], true)];
+            }
+        }
+        if (
+            array_key_exists('extra', $record)
+            && array_key_exists('token', $record['extra'])
+            && is_null($record['extra']['token'])
+        ) {
+            // This token contain a map of ["string" => "string"] values when the token is present; and is null when
+            //  the token is not set (yet). To avoid conflicts with ES index mapping settings, unset the token when
+            //  it's null.
+            unset($record['extra']['token']);
+        }
+        return parent::format($record);
+    }
+}
